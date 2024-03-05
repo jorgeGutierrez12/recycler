e4721855a62b591d43131027f612c1cadd4c0b47
Jorge <jorge.gutierrez4202@alumnos.udg.mx>
2024-03-05 14:45:13 -0600

    cetes

--- app/src/main/java/gutierrez/jorge/recycler/MainActivity.kt
+++ app/src/main/java/gutierrez/jorge/recycler/MainActivity.kt
@@ -15,10 +15,12 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var etTitulo: EditText
+    lateinit var etNombre: EditText
     lateinit var listaTareas:RecyclerView
     lateinit var adapter:TareasAdapter
     lateinit var skBarra : SeekBar
+
+
 
     private val tareasViewModel:TareasViewModel by viewModels()
 
@@ -26,16 +28,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        etTitulo = findViewById(R.id.etNombre)
+        etNombre = findViewById(R.id.etNombre)
         skBarra = findViewById(R.id.skBarra)
-
         listaTareas = findViewById(R.id.rvTareas)
-
-        /*tareasViewModel.elementos.add(
-            Tarea("",
-                "",
-                false)
-        */)
 
         adapter = TareasAdapter(tareasViewModel.elementos)
 
@@ -43,18 +38,36 @@ class MainActivity : AppCompatActivity() {
         listaTareas.layoutManager = GridLayoutManager(this,
             1)
 
-        val i = 0
-        skBarra.progress
 
 
-        /*btnGuardar.setOnClickListener {
+        skBarra.setOnSeekBarChangeListener(object :  SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val canti = etNombre.text.toString()
+                tareasViewModel.elementos.clear()
+                var pago = canti.toDouble()
+                var mes = 0
+                for (i in 0 until progress) {
+                    mes = mes + 1
+                    pago = pago + (pago * 0.11)
+                    val nuv =pago.toString()
+                    tareasViewModel.elementos.add(Tarea("Interes total: $nuv","Mes: $mes", false))
+                    adapter.notifyDataSetChanged()
+
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+            /*btnGuardar.setOnClickListener {
 
             val titulo = etTitulo.text.toString()
             tareasViewModel.elementos.add(Tarea(titulo,titulo,false))
 
             adapter.notifyDataSetChanged()
             Toast.makeText(this,"${tareasViewModel.elementos.size}",Toast.LENGTH_SHORT).show()
-       */ }
+        }*/
 
-    }
+        }
 }
